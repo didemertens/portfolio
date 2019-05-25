90aef5fc575a0b350497c07e41525fda62bb49aa
Dide Mertens <d.g.j.mertens@gmail.com>
2019-05-25 16:19:02 +0100

    added jquery to not refresh page after submitting form

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,1 +1,21 @@
-contact.js
+$(document).on('submit','#contact-bottom-form',function(e){
+  e.preventDefault();
+
+  // console.log($('#id_name').val())
+  // console.log($('#id_from_email').val())
+  // console.log($('#id_message').val())
+
+  $.ajax({
+    type:'POST',
+    url:'/email/',
+    data:{
+      name: $('#id_name').val(),
+      email: $('#id_from_email').val(),
+      message: $('#id_message').val(),
+      csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+    },
+    success:function(){
+      alert('Thank you for your message.');
+    }
+  });
+});
