def84de4134ddb9e4f3721cda7e0ba884956b4ef
Dide Mertens <d.g.j.mertens@gmail.com>
2019-05-25 18:07:36 +0100

    changed contact btn

--- basic_app/forms.py
+++ basic_app/forms.py
@@ -5,8 +5,8 @@ class ContactForm(forms.Form):
   from_email = forms.EmailField(required=True)
   message = forms.CharField(widget=forms.Textarea, required=True)
 
-  # def __init__(self, *args, **kwargs):
-  #   super(ContactForm, self).__init__(*args, **kwargs)
-  #   self.fields['name'].label = "Your name"
-  #   self.fields['from_email'].label = "Your email"
-  #   self.fields['message'].label = "Message"
+  def __init__(self, *args, **kwargs):
+    super(ContactForm, self).__init__(*args, **kwargs)
+    self.fields['name'].label = "Your name"
+    self.fields['from_email'].label = "Your email"
+    self.fields['message'].label = "Message"
