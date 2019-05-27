b6624d275b480bd26611a03690d53143a28c0797
Dide Mertens <d.g.j.mertens@gmail.com>
2019-05-27 16:26:21 +0100

    sendgrid heroku

--- basic_app/views.py
+++ basic_app/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from .forms import ContactForm
 
+
 # Create your views here.
 def index(request):
   return render(request,'index.html')
