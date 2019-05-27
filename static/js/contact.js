47cd80280adb1bc6a03e0e4320622a584d465054
Dide Mertens <d.g.j.mertens@gmail.com>
2019-05-27 16:44:23 +0100

    added js

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,18 +1,18 @@
 $(document).on('submit','#contact-bottom-form',function(e){
-  // e.preventDefault();
+  e.preventDefault();
 
-  // $.ajax({
-  //   type:'POST',
-  //   url:'email/',
-  //   data:{
-  //     name: $('#id_name').val(),
-  //     from_email: $('#id_from_email').val(),
-  //     message: $('#id_message').val(),
-  //     csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
-  //   },
-  //   success:function(){
-  //     alert('Thank you for your message.');
-  //   }
-  // });
-  // $("#contact-bottom-form")[0].reset();
+  $.ajax({
+    type:'POST',
+    url:'email/',
+    data:{
+      name: $('#id_name').val(),
+      from_email: $('#id_from_email').val(),
+      message: $('#id_message').val(),
+      csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+    },
+    success:function(){
+      alert('Thank you for your message.');
+    }
+  });
+  $("#contact-bottom-form")[0].reset();
 });
