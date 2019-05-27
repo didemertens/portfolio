a2182ef8083dc2c094e952e25346cb5c50cb382e
Dide Mertens <d.g.j.mertens@gmail.com>
2019-05-27 17:34:45 +0100

    added utf tag to header"

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,9 +132,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #email form
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
+# old
 # EMAIL_HOST = 'smtp.sendgrid.net'
 # EMAIL_HOST_USER = 'apikey'
 # EMAIL_HOST_PASSWORD = os.path.join('secret_settings')
