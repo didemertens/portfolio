db9122923de86af2c390a596caa1909d9791bbc5
Dide Mertens <d.g.j.mertens@gmail.com>
2019-08-01 10:39:13 +0200

    JS make animation wait

--- static/js/scroll-top.js
+++ static/js/scroll-top.js
@@ -1,11 +1,13 @@
 // Load img and then animation
 
-const img = document.querySelector("#laptop-image");
-document.body.classList.add('js-loading');
-img.addEventListener("load", removeLoadingClass);
-function removeLoadingClass() {
-  document.body.classList.remove('js-loading');
-}
+document.documentElement.className += " js-loading";
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+    document.documentElement.className = document.documentElement.className.replace("js-loading","");
+  };
+
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -16,10 +18,10 @@ function scrollFunction() {
   } else {
     document.getElementById("myBtn").style.display = "none";
   }
-}
+};
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
